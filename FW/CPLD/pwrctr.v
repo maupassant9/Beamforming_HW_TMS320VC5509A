3d18b964c2fd029d58ac4b1a805fe2212ad72c9e
dong xia <xiadong.br@gmail.com>
2021-03-27 08:20:01 -0300

    Elaborate Verilog HDL project

--- FW/CPLD/pwrctr.v
+++ FW/CPLD/pwrctr.v
@@ -16,10 +16,32 @@ module pwrctr(
 	//boot image rd
 	output eeprom_sclk,
 	output eeprom_mosi,
-	output cpld_mem_cs,
-	input eeprom_miso
+	output eeprom_mem_cs,
+	input eeprom_miso,
+	
+	//dsp side spi pins for boot
+	input sclk,
+	input mosi,
+	input cs,
+	output miso
 	
 );
-
-	assign pwr_en = clk_in;
+	
+	reg highreg;
+	//connect the dsp spi pins and eeprom spi pins
+	assign eeprom_sclk = sclk;
+	assign eeprom_mosi = mosi;
+	assign eeprom_mem_cs = cs;
+	assign miso = eeprom_miso;
+	
+	//wait for first clk, and set bootcfg
+	assign pwr_en = highreg;
+	assign bootcfg[0] = highreg;
+	assign bootcfg[1] = !highreg;
+	assign bootcfg[2] = !highreg;
+	assign bootcfg[3] = highreg;
+	always @ (posedge clk_in) begin
+		highreg = 1;
+	end 	
+	
 endmodule
